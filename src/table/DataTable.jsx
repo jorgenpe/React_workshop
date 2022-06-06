8b494072163a348e6514b4fc1bdec1f5fac929d5
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-06-06 20:25:18 +0200

    Base line created

---
+++ src/table/DataTable.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+
+export default function DataTable() {
+    // the initial state with objects containing the student data/properties.
+
+    // showDetails state for displaying student details with a boolean,the button onclick function displayData returns a true value and the properties
+
+    // functionconponent that returns table header
+
+
+    // functionconponent 
+
+
+    // functionconponent action 
+
+
+    // functionconponent 
+}
