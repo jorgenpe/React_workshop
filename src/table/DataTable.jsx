ced642263673a89f18b95eac294cb748373f442e
jorgenpe <82228083+jorgenpe@users.noreply.github.com>
2022-06-09 08:16:41 +0200

    update workshop 3

--- src/table/DataTable.jsx
+++ src/table/DataTable.jsx
@@ -4,13 +4,13 @@ import React, { useState } from "react";
 export default function DataTable() {
     // the initial state with objects containing the student data/properties.
     const Person = () =>{
-        const initPerson = {Id:"", FirstName:"" , LastName:"", Email:"", Title:""}
+        const initPerson = {Id:0, FirstName:"" , LastName:"", age:0, birthdate: "", country:"", city:"" }
         const [person, setPerson] = useState(initPerson);
 
         return (
             <div>
                 <b>
-                    {person.FirstName} - {person.LastName} - {person.Email} - {person.Title}
+                    {person.FirstName} - {person.LastName} - {person.age} 
                 </b>
             </div>
         );
@@ -21,7 +21,7 @@ export default function DataTable() {
 
     }
 
-    
+
     // functionconponent that returns table header
     const tableHeader = () =>{
         return (
@@ -35,10 +35,25 @@ export default function DataTable() {
     };
 
     // functionconponent 
-    const studentList = () =>{
+    const studentList =  [
+        {
+            Id: 1, FirstName:"Anna" , LastName:"Karlson", age:27, birthdate: "", country:"Sverige", city:"Bromölla"
+        }
+        ,{
+            Id:2, FirstName:"Anders" , LastName:"Olsson", age:21, birthdate: "", country:"Finland", city:"Helsingfors"
+        }
+        ,{
+            Id:3, FirstName:"Karin" , LastName:"", age:52, birthdate: "", country:"Sverige", city:"Malmö"
+        },
+        {
+            Id:4, FirstName:"Oskar" , LastName:"Nilsson", age:45, birthdate: "", country:"Sverige", city:"Växjö"
+        }
+        ,
+        {
+            Id:5, FirstName:"Ann" , LastName:"Svensson", age:35, birthdate: "", country:"Danmark", city:"Helsingör"
+        }
+    ];
 
-
-    };
 
     // functionconponent action 
     const tableAction = () =>{
